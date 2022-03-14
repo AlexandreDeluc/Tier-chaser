91798883e3b714fa32488904acfb0c6e4f554d2b
Nadaud <clemence.nadaud@isen.yncrea.fr>
2022-03-14 14:23:55 +0100

    integration bdd firestore

--- app/src/main/java/fr/isen/deluc/tierchaser/MapsActivity.kt
+++ app/src/main/java/fr/isen/deluc/tierchaser/MapsActivity.kt
@@ -1,15 +1,14 @@
 package fr.isen.deluc.tierchaser
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-
-import com.google.android.gms.maps.CameraUpdateFactory
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.firestore.FirebaseFirestore
 import fr.isen.deluc.tierchaser.databinding.ActivityMapsBinding
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -17,7 +16,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
 
-    val TAG = "MapsActivity"
+    private val TAG = "MapsActivity"
+    val db = FirebaseFirestore.getInstance()
+    var arMarkets= arrayListOf<Market>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,21 +35,26 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        db.collection("products")
+        db.collection("markets")
             .addSnapshotListener { result, e ->
-                if (e != null) {
+                if(e != null) {
                     Log.w(TAG, "Listen failed", e)
                     return@addSnapshotListener
                 }
-                arProducts.clear()
-                arProducts.addAll(result!!.toObjects(Product::class.java))
-                rv_list.adapter= List2Adapter(arProducts,context!!)
+                mMap.clear()
+                arMarkets.clear()
+                arMarkets.addAll(result!!.toObjects(Market::class.java))
 
+                for(markets in arMarkets){
+                    val geoPosition = LatLng(markets.latitude, markets.longitude)
+                    mMap.addMarker(MarkerOptions().position(geoPosition).title(markets.name))
+                }
             }
 
-        // Add a marker in Sydney and move the camera
+        //Add a marker in Sidney
         /*val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))*/
+        mMap.addMarker(MarkerOptions().position(sydney).title("Sidney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney)) //permet de centrer la carte sur un point
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(20f)) //permet de zoomer*/
     }
 }
