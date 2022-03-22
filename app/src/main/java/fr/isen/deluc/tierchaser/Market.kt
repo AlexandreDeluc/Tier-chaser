3d9629b9f41086a2c8cfb2180b46a35ed7b397c8
Nadaud <clemence.nadaud@isen.yncrea.fr>
2022-03-22 16:56:15 +0100

    lien bdd et marker

--- app/src/main/java/fr/isen/deluc/tierchaser/Market.kt
+++
@@ -1,7 +0,0 @@
-package fr.isen.deluc.tierchaser
-
-import java.io.Serializable
-
-class Market (var name:String, var avatar:String, var latitude:Double, var longitude:Double):Serializable{
-    constructor():this("", "",0.0, 0.0)
-}
