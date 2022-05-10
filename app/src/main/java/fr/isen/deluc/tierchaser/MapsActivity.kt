7a240ff12d642b330f51f5100648c569ae5c69b4
Nadaud <clemence.nadaud@isen.yncrea.fr>
2022-05-10 15:43:37 +0200

    commit ajout des ressources

--- app/src/main/java/fr/isen/deluc/tierchaser/MapsActivity.kt
+++ app/src/main/java/fr/isen/deluc/tierchaser/MapsActivity.kt
@@ -1,6 +1,11 @@
 package fr.isen.deluc.tierchaser
 
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
+import android.text.InputType
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -11,8 +16,8 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
-
 import fr.isen.deluc.tierchaser.databinding.ActivityMapsBinding
+
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -38,6 +43,25 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     }
 
+    private fun goToAnotherActivity() {
+        val intent = Intent(this, PublishActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun showDialog() {
+        val builder: AlertDialog.Builder = android.app.AlertDialog.Builder(this)
+        builder.setTitle("Souhaitez-vous changer de page?")
+
+        builder.setPositiveButton("Post", DialogInterface.OnClickListener { dialog, which ->
+            goToAnotherActivity()
+        })
+        builder.setNegativeButton(
+            "Carte",
+            DialogInterface.OnClickListener { dialog, which -> dialog.cancel() })
+        builder.show()
+    }
+
+
     private fun setNewMarket(map: GoogleMap){
 
             val database = Firebase.database("https://tierchaser-default-rtdb.firebaseio.com/").getReference("Markets")
@@ -46,24 +70,27 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
                 for (ds in it.children) {
 
-                Toast.makeText(this,"La base de donnees a été lue", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this,"La base de donnees a été lue", Toast.LENGTH_SHORT).show()
 
-                    if (it.exists()) {
+                            if (it.exists()) {
 
-                            val latitude = ds.child("latitude").getValue(Double::class.java) as Double
-                            val longitude = ds.child("longitude").getValue(Double::class.java) as Double
-                            val name = ds.child("name").getValue(String::class.java)
-                            //val marker = ds.child("imageUrl").getValue(DataSnapshot::class.java) as Int
+                                val latitude = ds.child("latitude").getValue(Double::class.java) as Double
+                                val longitude = ds.child("longitude").getValue(Double::class.java) as Double
+                                val name = ds.child("name").getValue(String::class.java)
+                                val cities = LatLng(latitude, longitude)
+                                val zoomLevel = 15f // f : float number
 
-                            val cities = LatLng(latitude, longitude)
-                            val zoomLevel = 15f // f : float number
+                                map.addMarker(MarkerOptions().position(cities).title(name))
+                                map.moveCamera(CameraUpdateFactory.newLatLngZoom(cities, zoomLevel))
 
-                            map.addMarker(MarkerOptions().position(cities).title(name))
-                            map.moveCamera(CameraUpdateFactory.newLatLngZoom(cities, zoomLevel))
 
-                        //.icon(BitmapDescriptorFactory.fromResource(marker))
+                                mMap.setOnMarkerClickListener { marker -> // on marker click we are getting the title of our marker
+                                    showDialog()
+                                    false
+                                }
 
-                    }
+                            }
+
                 }
             }
     }
---
+++ app/src/main/java/fr/isen/deluc/tierchaser/ShowObjectActivity.kt
@@ -0,0 +1,11 @@
+package fr.isen.deluc.tierchaser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowObjectActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_object)
+    }
+}
