c85cfd658658daf8e49c7af7fef61dd512b645e8
Nadaud <clemence.nadaud@isen.yncrea.fr>
2022-05-12 11:36:32 +0200

    commit creation adapter

---
+++ app/src/main/java/fr/isen/deluc/tierchaser/adapter/ObjectAdapter.kt
@@ -0,0 +1,32 @@
+package fr.isen.deluc.tierchaser.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import fr.isen.deluc.tierchaser.R
+
+class ObjectAdapter : RecyclerView.Adapter<ObjectAdapter.ViewHolder>() {
+
+    //boite pour ranger tout les composants à controler
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        val objectImage = view.findViewById<ImageView>(R.id.image_item)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.item_horizontal_object, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        return 5
+    }
+}
