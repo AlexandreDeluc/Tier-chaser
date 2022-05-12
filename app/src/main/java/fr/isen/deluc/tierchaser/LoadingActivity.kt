f9dac8765ae2f7cb95857232a1538a26c5058a5e
Nadaud <clemence.nadaud@isen.yncrea.fr>
2022-05-12 16:14:10 +0200

    first commit implementation loading + login

---
+++ app/src/main/java/fr/isen/deluc/tierchaser/LoadingActivity.kt
@@ -0,0 +1,31 @@
+package fr.isen.deluc.tierchaser
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import fr.isen.deluc.tierchaser.databinding.ActivityLoadingBinding
+
+class LoadingActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLoadingBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoadingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        Handler().postDelayed(
+            {
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+                finish()
+            },
+            3000
+        )
+
+    }
+}
---
+++ app/src/main/java/fr/isen/deluc/tierchaser/LoginActivity.kt
@@ -0,0 +1,113 @@
+package fr.isen.deluc.tierchaser
+
+
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.InputType
+import android.text.TextUtils
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import fr.isen.deluc.tierchaser.databinding.ActivityLoginBinding
+
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginBtn.setOnClickListener {
+            when {
+                TextUtils.isEmpty(binding.email.text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(this, "Please enter an email", Toast.LENGTH_LONG).show()
+                }
+                TextUtils.isEmpty(binding.password.text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(this, "Please enter a password", Toast.LENGTH_LONG).show()
+                }
+
+                else -> {
+                    signIn()
+                }
+            }
+        }
+
+        binding.noAccountBtn.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        binding.forgotPassword.setOnClickListener {
+            showDialog()
+        }
+    }
+
+    private fun showDialog() {
+        val builder: AlertDialog.Builder = android.app.AlertDialog.Builder(this)
+        builder.setTitle("Reset your password")
+
+        val input = EditText(this)
+        input.setHint("Enter your mail address")
+        input.inputType = InputType.TYPE_CLASS_TEXT
+        builder.setView(input)
+
+        builder.setPositiveButton("OK", DialogInterface.OnClickListener { dialog, which ->
+            var emailInput = input.text.toString()
+            Firebase.auth.sendPasswordResetEmail(emailInput)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "Email sent", Toast.LENGTH_LONG).show()
+                    }
+                }
+
+        })
+        builder.setNegativeButton(
+            "Cancel",
+            DialogInterface.OnClickListener { dialog, which -> dialog.cancel() })
+
+        builder.show()
+    }
+
+    private fun signIn() {
+        val email: String = binding.email.text.toString().trim() { it <= ' ' }
+        val password: String = binding.password.text.toString().trim() { it <= ' ' }
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+
+                val firebaseUser: FirebaseUser? = FirebaseAuth.getInstance().currentUser
+
+                if (task.isSuccessful) {
+
+                    if (firebaseUser?.isEmailVerified!!) {
+
+                        val intent = Intent(this, HomeActivity::class.java)
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+
+                        intent.putExtra("user_id", FirebaseAuth.getInstance().currentUser!!.uid)
+                        intent.putExtra("email_id", email)
+
+                        Toast.makeText(this, "Account is verified, you are logged in", Toast.LENGTH_LONG).show()
+                        finish()
+                        startActivity(intent)
+                    }
+                    else{
+                        Toast.makeText(this, "Account not verified", Toast.LENGTH_LONG).show()
+                    }
+                }
+                else {
+                    Toast.makeText(this, task.exception!!.message.toString(), Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+}
